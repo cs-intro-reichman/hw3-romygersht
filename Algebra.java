0c94b3feced2361fd235f249c7ef5eb28d335386
Romy Gersht <romygersht@Romys-MacBook-Air.local>
2024-11-26 13:31:31 +0200

    hw3

--- Algebra.java
+++ Algebra.java
@@ -150,7 +150,7 @@ public class Algebra {
             {
               count ++;
               return count; 
-            }
+            } 
             if (sum > a)
             { 
                 return count;
